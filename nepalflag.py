ce8e729437f75d6066b439b966306f47a3b12849
Sushant-Bhattaraii <sushantbhattarai2063@gmail.com>
2024-12-15 21:45:48 +0545

    Nepal flag

---
+++ nepalflag.py
@@ -0,0 +1,13 @@
+
+# Nepal flag
+for i in range(4):
+    for j in range(i+1):
+        print("* ",end="")
+    print()
+
+for i in range(6):
+    for j in range(i+1):
+        print("* ",end="")
+    print()
+for i in range(3):
+    print("*")
